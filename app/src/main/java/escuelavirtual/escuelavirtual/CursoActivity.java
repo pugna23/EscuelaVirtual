c3018240d7358d26d2dd201304e38a066cb8bad4
cgmanoja <cgmanoja@gmail.com>
2018-05-12 03:55:16 -0300

    Prueba de RecyclerViews para Cursos y Ejercicios

--- app/src/main/java/escuelavirtual/escuelavirtual/CursoActivity.java
+++ app/src/main/java/escuelavirtual/escuelavirtual/CursoActivity.java
@@ -1,6 +1,5 @@
 package escuelavirtual.escuelavirtual;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,12 +9,12 @@ import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class CursoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_curso);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
@@ -26,20 +25,8 @@ public class MainActivity extends AppCompatActivity {
         ejecicios.add(new Ejercicio(R.string.ejercicio_2));
         ejecicios.add(new Ejercicio(R.string.ejercicio_3));
 
-        List<Curso> cursos = new ArrayList<>();
-        cursos.add(new Curso(R.string.curso_1, ejecicios));
-        cursos.add(new Curso(R.string.curso_2, ejecicios));
-        cursos.add(new Curso(R.string.curso_3, ejecicios));
-        cursos.add(new Curso(R.string.curso_4, ejecicios));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        recyclerView.setAdapter(new ModelAdapter(cursos));
+        recyclerView.setAdapter(new ModelAdapterEjercicio(ejecicios));
     }
-
-    public void gotoCurso(View view){
-
-        Intent intent = new Intent(this, CursoActivity.class);
-        startActivity(intent);
-    }
-
 }
